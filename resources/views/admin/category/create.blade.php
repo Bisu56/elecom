97fce97d29540f3c895fbe49e2ebf8dfad8d77f6
Bisu56 <rajbanshibishal441@gmail.com>
2026-02-06 22:51:55 +0545

    category form

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,5 +1,22 @@
 @extends('admin.layouts.layout')
-@section('admin_page_title')Category Create@endsection
+@section('admin_page_title')Category Create
+@endsection
 @section('admin_layout')
-    <h1>Category Create Page</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Create Category</h4>
+                </div>
+                <div class="card-body">
+                    <form action="" method="POST">
+                        @csrf
+                        <label for="category_name" class="fw-bold mb-2">Category Name</label>
+                        <input type="text" name="category_name" id="category_name" class="form-control mb-3" placeholder="Enter category name">
+                        <button type="submit" class="btn btn-primary w-100 mt-3">Create Category</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
